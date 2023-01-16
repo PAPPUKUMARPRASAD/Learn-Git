57191ffd4113ae03292a312dbba4558fcb36f6b9
Sameer Kumar Prasad <sameerkumar2812@gmail.com>
2023-01-16 13:59:21 +0530

    modified sum.js

--- sum.js
+++ sum.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-    let result=sum(15,25);
+    let result=sum(15,25,20);
     console.log(result);
 })
-function sum(a,b)
+function sum(a,b,c)
     {
-    let sum=a+b;
+    let sum=a+b+c;
     return sum;
     }
